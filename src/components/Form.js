739c684c49e6d07461b871fe731df3f2f82ec8b5
colev1 <colevanacore@gmail.com>
2019-07-10 14:15:14 -0400

    Basic styling for filter container

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,8 @@ import '../styling/Form.css';
 const Form = ({filter, changeFilter}) => {
     return (
       <form>
-        <input type="text" placeholder="Filter articles" value={filter} onChange={(e) => changeFilter(e)}/>
+        <p> filter by keyword:  </p>
+        <input type="text" value={filter} onChange={(e) => changeFilter(e)}/>
       </form>
     )
 }
