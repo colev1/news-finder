15dea2d53bf4f0cefae87ecff0fee6b2500a59b7
colev1 <colevanacore@gmail.com>
2019-07-09 21:21:11 -0400

    Clean up fetch all and add titles array to state of app

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../styling/App.css';
+import cleanArticles from '../helper'
 
-function App() {
-  const fetchNews = () => {
-    fetch('https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=l0v3Eo88AnIzzFYKaws93M7gOCQ9UBjE')
-      .then(response => response.json())
-      .then(result => console.log(result.results))
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      titles: []
+    }
   }
 
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        News app
-        <button onClick={fetchNews}> press </button>
-      </header>
-    </div>
-  );
+  fetchNews = () => {
+    fetch('https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=l0v3Eo88AnIzzFYKaws93M7gOCQ9UBjE')
+      .then(response => response.json())
+      .then(result => cleanArticles(result.results))
+      .then(titles => this.setState({
+        titles
+      }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          News app
+          <button onClick={this.fetchNews}> press </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
