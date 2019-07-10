9c1b303bde8a817822e87592c12e2f4af8605f9d
colev1 <colevanacore@gmail.com>
2019-07-10 11:40:34 -0400

    Add new fetched articles to the display container

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../styling/App.css';
 import cleanArticles from '../helper';
 import Main from './Main';
@@ -14,30 +14,40 @@ class App extends Component {
   }
 
   fetchNews = () => {
-    const {limit, offset} = this.state;
+    const { limit, offset } = this.state;
     let url = `https://api.nytimes.com/svc/news/v3/content/nyt/all.json?api-key=l0v3Eo88AnIzzFYKaws93M7gOCQ9UBjE&limit=${limit}&offset=${offset}`
     fetch(url)
       .then(response => response.json())
       .then(result => cleanArticles(result.results))
-      .then(articles => this.setState({
-        articles
-      }))
+      .then(articles => this.displayArticles(articles))
       .catch(err => console.log(err))
+  }
+
+  displayArticles = (articles) => {
+    if(!this.state.offset) {
+      this.setState({
+        articles
+      })
+    } else {
+      this.setState({
+        articles: this.state.articles.concat(articles)
+      })
+    }
   }
 
   showMoreArticles = () => {
     this.setState({
       offset: this.state.offset + 10
     })
-    this.fetchNews()
+    this.fetchNews();
   }
 
   render() {
     return (
       <div className="App">
-          News app
+        News app
           <button onClick={this.fetchNews}> press </button>
-          <Main articles={this.state.articles} showMoreArticles={this.showMoreArticles}/>
+        <Main articles={this.state.articles} showMoreArticles={this.showMoreArticles} />
       </div>
     );
   }
