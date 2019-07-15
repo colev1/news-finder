c50c19a07709aadc2a4063d3a84fb3ca6cd01320
colev1 <colevanacore@gmail.com>
2019-07-14 20:41:47 -0400

    Move source to nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,6 +7,9 @@ const Nav = ({ fetchNews }) => {
     <div className="nav-container">
       <h1> Click to load top New York Times articles </h1>
       <img onClick={fetchNews} className="nav-button" src={arrow} alt="Arrow right button." />
+      <a href="https://developer.nytimes.com/" className="source">
+        api: https://developer.nytimes.com/
+      </a>
     </div>
   )
 }
